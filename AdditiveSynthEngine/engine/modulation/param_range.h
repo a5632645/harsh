32fd5efb1198bbeee35cfd8d48188a226b0b805c
a5632645 <3022602213@qq.com>
2024-05-19 16:15:58 +0800

    param range

---
+++ AdditiveSynthEngine/engine/modulation/param_range.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <cmath>
+
+namespace mana {
+struct ParamRange {
+    inline constexpr float ConvertFrom01(float x) {
+        return std::lerp(vmin, vmax, x);
+    }
+
+    inline constexpr float ConvertTo01(float x) {
+        return (x - vmin) / (vmax - vmin);
+    }
+
+    constexpr ParamRange(float vmin, float vmax, float vdefault, float vstep) : vmin(vmin), vmax(vmax), vdefault(vdefault), vstep(vstep) {}
+    constexpr ParamRange(float vmin, float vmax) : ParamRange(vmin, vmax, (vmin + vmax) / 2, 0.0f) {}
+    constexpr ParamRange(float vmin, float vmax, float vdefault) : ParamRange(vmin, vmax, vdefault, 0.0f) {}
+
+    float vmax;
+    float vmin;
+    float vdefault;
+    float vstep;
+};
+}
