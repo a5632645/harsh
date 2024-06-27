36de27137abc909f1fc0fdb7c8a233a8f52c2221
a5632645 <3022602213@qq.com>
2024-06-27 17:47:02 +0800

    if constexpr should not with constant_eval

--- AdditiveSynthEngine/synthsis/utli/convert.h
+++ AdditiveSynthEngine/synthsis/utli/convert.h
@@ -6,7 +6,7 @@
 namespace mana::utli {
 template<std::floating_point T>
 inline static constexpr T Exp2(T x) {
-    if constexpr (std::is_constant_evaluated()) {
+    if (std::is_constant_evaluated()) {
         return gcem::exp(0.69314718055994530941723212145818f * x);
     }
     else {
@@ -16,7 +16,7 @@ inline static constexpr T Exp2(T x) {
 
 template<std::floating_point T>
 inline static constexpr T DbToGain(T db) {
-    if constexpr (std::is_constant_evaluated()) {
+    if (std::is_constant_evaluated()) {
         return gcem::exp(0.11512925464970228420089957273422f * db);
     }
     else {
@@ -38,7 +38,7 @@ inline static constexpr T GainToDb(T gain) {
     if (gain < min_gain) {
         return kMinDb;
     }
-    if constexpr (std::is_constant_evaluated()) {
+    if (std::is_constant_evaluated()) {
         return 20.0f * gcem::log10(gain);
     }
     else {
@@ -48,7 +48,7 @@ inline static constexpr T GainToDb(T gain) {
 
 template<std::floating_point T>
 inline static constexpr T PitchToFreq(T pitch) {
-    if constexpr (std::is_constant_evaluated()) {
+    if (std::is_constant_evaluated()) {
         return gcem::exp(pitch * 0.05776226504666210911810267678818f) * 8.1758f;
     }
     else {
@@ -58,7 +58,7 @@ inline static constexpr T PitchToFreq(T pitch) {
 
 template<std::floating_point T>
 inline static constexpr T FreqToPitch(T freq) {
-    if constexpr (std::is_constant_evaluated()) {
+    if (std::is_constant_evaluated()) {
         return gcem::log(freq / 8.1758f) / 0.05776226504666210911810267678818f;
     }
     else {
@@ -68,7 +68,7 @@ inline static constexpr T FreqToPitch(T freq) {
 
 template<std::floating_point T>
 inline static constexpr T Calc1stSmoothFilterCoeff(T time_second, T update_rate) {
-    if constexpr (std::is_constant_evaluated()) {
+    if (std::is_constant_evaluated()) {
         return gcem::exp(-1.0f / (update_rate * time_second));
     }
     else {
@@ -78,7 +78,7 @@ inline static constexpr T Calc1stSmoothFilterCoeff(T time_second, T update_rate)
 
 template<std::floating_point T>
 inline static constexpr T Calc1stSmoothFilterCoeffByDecayRate(T db_per_second, T update_rate) {
-    if constexpr (std::is_constant_evaluated()) {
+    if (std::is_constant_evaluated()) {
         return gcem::exp(-0.11512925464970228420089957273422f * db_per_second / update_rate);
     }
     else {
@@ -89,7 +89,7 @@ inline static constexpr T Calc1stSmoothFilterCoeffByDecayRate(T db_per_second, T
 template<std::floating_point T>
 inline static constexpr T RatioToPitch(T ratio, T base_pitch) {
     ratio = std::max(ratio, 0.0001f);
-    if constexpr (std::is_constant_evaluated()) {
+    if (std::is_constant_evaluated()) {
         return base_pitch + 12.0f * gcem::log2(ratio);
     }
     else {
